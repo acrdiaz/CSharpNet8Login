7a4abb6234b8034d3800749c3439980b0c95fee3
acrdiaz <acrdiaz@msn.com>
2025-02-25 23:16:30 -0400

    add login view model

---
+++ CSharpNet8Login/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using CSharpNet8Login.Entities;
+using CSharpNet8Login.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CSharpNet8Login.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public AccountController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Registration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Registration(RegistrationViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                UserAccount account = new UserAccount
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    Email = model.Email,
+                    Username = model.Username,
+                    Password = model.Password
+                };
+                _context.UserAccounts.Add(account);
+                _context.SaveChanges();
+
+                ModelState.Clear();
+
+                //return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+    }
+}
---
+++ CSharpNet8Login/Models/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CSharpNet8Login.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Username is required.")]
+        [MaxLength(20, ErrorMessage = "Max. 20 charactesr allowed.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(20, MinimumLength = 5, ErrorMessage = "Max. 20 or min. 5 charactesr allowed.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}

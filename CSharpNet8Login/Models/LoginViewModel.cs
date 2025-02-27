981e56d1c9e862afc357644a5600e457568d001f
acrdiaz <acrdiaz@msn.com>
2025-02-27 12:39:53 -0400

    fix typos, recreate registration form use correct login view model

--- CSharpNet8Login/Models/LoginViewModel.cs
+++ CSharpNet8Login/Models/LoginViewModel.cs
@@ -6,12 +6,12 @@ namespace CSharpNet8Login.Models
     public class LoginViewModel
     {
         [Required(ErrorMessage = "Username or Email is required.")]
-        [MaxLength(20, ErrorMessage = "Max. 20 charactesr allowed.")]
+        [MaxLength(20, ErrorMessage = "Max. 20 characters allowed.")]
         [DisplayName("Username or Email")]
         public string UsernameOrEmail { get; set; }
 
         [Required(ErrorMessage = "Password is required.")]
-        [StringLength(20, MinimumLength = 5, ErrorMessage = "Max. 20 or min. 5 charactesr allowed.")]
+        [StringLength(20, MinimumLength = 5, ErrorMessage = "Max. 20 or min. 5 characters allowed.")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
     }
